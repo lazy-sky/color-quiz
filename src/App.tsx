c68f209ee9531c6ecf3b2d14b6ce66a5e4d33e09
lazy-sky <1cielo@naver.com>
2022-02-01 05:09:15 +0900

    refact: Move states to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
+
 import ScoreBoard from './components/ScoreBoard';
 import ColorBoard from './components/ColorBoard';
 
 function App() {
+  const [stage, setStage] = useState(1);
+  const [remainingTime, setRemainingTime] = useState(15);
+  const [score, setScore] = useState(0);
+
   return (
     <>
       <h1>Color Quiz</h1>
-      <ScoreBoard />
-      <ColorBoard /> 
+      <ScoreBoard 
+        stage={stage} 
+        score={score}
+      />
+      <ColorBoard 
+        stage={stage} 
+        setStage={setStage}
+        remainingTime={remainingTime}
+        setRemainingTime={setRemainingTime}
+      /> 
     </>
   );
 }
