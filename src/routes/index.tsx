5ef11fe8063c710cbd5ca2f6133be774b128bb5a
lazy-sky <1cielo@naver.com>
2023-01-08 22:35:22 +0900

    Refactor: Change top level router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Navigation from '../components/Navigation'
 import HomePage from './HomePage'
@@ -9,13 +9,13 @@ import styles from './routes.module.scss'
 const App = () => {
   return (
     <div className={styles.app}>
-      <Router>
+      <BrowserRouter>
         <Navigation />
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/rank' element={<RankingPage />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   )
 }
